be97ee2b36a147a1c51da996f84241012ed7f832
yume1610rein <taikousi504@gmail.com>
2022-05-01 01:46:40 +0900

    名前修正 ファイル開始位置とデータサイズの書き出しが間違っていたので修正

--- FileArciverForDX12Library/Program.cs
+++ FileArciverForDX12Library/Program.cs
@@ -5,15 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace FileArciverForDX12Library
+namespace FileArchiverForDX12Library
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //D&Dされたパスリスト
             List<string> paths = new List<string>();
-            List<string> files = new List<string>();
+            //パスリストに含まれる全てのファイルリスト
+            Dictionary<string, List<string>> files = new Dictionary<string, List<string>>();
+            //出力名
             string dstName;
+            //ファイルサイズコンテナ
+            List<int> dataSize = new List<int>();
 
             //実行ファイルに直接D&Dされていなかった場合
             if (args.Length == 0)
@@ -21,7 +26,7 @@ namespace FileArciverForDX12Library
                 //パスを文字列として受け取る
                 Console.WriteLine("アーカイブしたいフォルダをドラッグ&ドロップして実行してください。");
                 Console.WriteLine("何かキーを押すと終了します...");
-                Console.ReadLine();
+                Console.ReadKey();
                 return;
             }
             else
@@ -32,24 +37,18 @@ namespace FileArciverForDX12Library
                     paths.Add(v);
                 }
 
-                //引数が1つなら出力名はフォルダ名.arkにする
-                if (args.Length == 1)
-                {
-                    string parentDir = args[0].Substring(args[0].LastIndexOf("\\") + 1);
-                    dstName = parentDir + ".ark";
-                }
-                //引数複数なら名前を指定させる
-                else
-                {
-                    Console.Write("出力名を入力してください。\n(拡張子記述は不要です)\nName:");
-                    dstName = Console.ReadLine();
-                    dstName += ".ark";
-                }
+                //出力名を指定させる
+                Console.Write("出力名を入力してください。\n(拡張子記述は不要です)\nName:");
+                dstName = Console.ReadLine();
+                dstName += ".ark";
             }
 
             //フォルダに格納されているすべてのファイルを列挙
             foreach (var v in paths)
             {
+                //リスト生成
+                files.Add(v, new List<string>());
+
                 //全ファイル取得
                 string[] tmp = Directory.GetFiles(v, "*", SearchOption.AllDirectories);
 
@@ -60,7 +59,7 @@ namespace FileArciverForDX12Library
                     string parentDir = v.Substring(v.LastIndexOf("\\") + 1);
                     string path = v2.Substring(v2.IndexOf(parentDir));
 
-                    files.Add(path);
+                    files[v].Add(path);
                 }
             }
 
@@ -68,17 +67,90 @@ namespace FileArciverForDX12Library
             string dstDir = args[0].Substring(0, args[0].LastIndexOf("\\"));
             StreamWriter sw = new StreamWriter(dstDir + "\\" + dstName);
 
-            //テスト出力
-            foreach (var v in files)
+            //ファイルを一つ一つ書き出し
+            string output = "";
+            string pwd = Environment.CurrentDirectory;
+
+            //データ部
+            foreach (var v1 in files)
             {
-                Console.WriteLine(v);
-                sw.WriteLine(v);
+                //カレントディレクトリを変更
+                Environment.CurrentDirectory = v1.Key.Substring(0, v1.Key.LastIndexOf("\\"));
+
+                foreach (var v2 in v1.Value)
+                {
+                    Console.Write(v2 + "を書き込みます...");
+
+                    //まずは全データ結合
+                    StreamReader sr = new StreamReader(v2);
+
+                    string addStr = sr.ReadToEnd();
+
+                    //データ追加
+                    output += addStr;
+
+                    //データ開始位置追加
+                    Encoding e = sr.CurrentEncoding;
+
+                    dataSize.Add(e.GetByteCount(addStr));
+
+                    sr.Close();
+
+                    Console.WriteLine("完了");
+                }
             }
 
-            sw.Close();
+            string header = "";
+            int index = 0;
+            int startPos = 0;
+            //ヘッダー部
+            foreach (var v1 in files)
+            {
+                //カレントディレクトリを変更
+                Environment.CurrentDirectory = v1.Key.Substring(0, v1.Key.LastIndexOf("\\"));
 
-            Console.WriteLine("何かキーを押すと終了します...");
-            Console.ReadLine();
+                foreach (var v2 in v1.Value)
+                {
+                    //ファイル名
+                    header += "\"" + v2.Replace("\\", "/") + "\"";
+                    //開始位置とデータサイズ挿入
+                    header += startPos + "," + dataSize[index];
+
+                    startPos += dataSize[index];
+                    index++;
+                }
+            }
+
+            //さいごにヘッダーサイズと識別子を挿入
+            const int IDENTIFIER_SIZE = 16;
+            string headerSize = (sw.Encoding.GetByteCount(header) + IDENTIFIER_SIZE).ToString();
+
+            while(headerSize.Length < 12)
+            {
+                headerSize = "0" + headerSize;
+            }
+
+            header = "ARCK" + headerSize + header;
+
+
+            Console.Write(dstDir + "にファイルを出力...");
+
+            //カレントディレクトリを変更
+            Environment.CurrentDirectory = pwd;
+
+            //書き出し
+            sw.Write(header);
+            sw.Write(output);
+
+            sw.Close();
+            paths.Clear();
+            files.Clear();
+            dataSize.Clear();
+
+            Console.WriteLine("完了");
+
+            Console.WriteLine("\nすべての処理が完了しました。\n何かキーを押すと終了します...");
+            Console.ReadKey();
         }
     }
 }
